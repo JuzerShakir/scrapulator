93efa724092bb13172f57878e2b7b4f84da71419
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-04-23 12:58:09 +0530

    import SVG icon assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+// import assets
+// import editIcon from "./assets/edit.svg";
+// import homeIcon from "./assets/home.svg";
+// import smallRupeeIcon from "./assets/small_rupee.svg";
+// import largeRupeeIcon from "./assets/large_rupee.svg";
+
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0)
