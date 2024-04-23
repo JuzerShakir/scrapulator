041fdd30c9536fcca7c7f2a51412df7789864f69
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-04-23 15:39:20 +0530

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,15 @@ import { useState } from "react";
 // import largeRupeeIcon from "./assets/large_rupee.svg";
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<header className="flex justify-center">
+				<h1 className="patrick-hand-sc-regular text-5xl text-yellow-700">
+					Scrapulator
+				</h1>
+			</header>
+		</>
+	);
 }
 
 export default App;
