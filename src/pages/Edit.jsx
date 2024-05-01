d96c991ad7bb3f6a1a9da2461c008e65977220ec
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-05-01 15:21:58 +0530

    add footer component

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Header from "./../components/Edit/Header";
 import ScrapItems from "../components/Edit/ScrapItems";
+import Footer from "./../components/Footer";
 import { scrapItemsList } from "../scrapItemsList";
 
 function Edit() {
@@ -11,6 +12,7 @@ function Edit() {
 		<>
 			<Header />
 			<ScrapItems items={items} setItems={setItems} />
+			<Footer />
 		</>
 	);
 }
