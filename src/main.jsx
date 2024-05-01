b5418566c45f2dde87ab0557a1e11ff7de07bc14
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-05-01 11:51:57 +0530

    define routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// pages
+import HomePage from "./pages/HomePage";
+import Edit from "./pages/Edit";
+
+// stylings
+import "./index.css";
+
+// routing
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+	{
+		path: "/edit",
+		element: <Edit />,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
