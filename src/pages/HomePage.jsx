ee20577d8a56880d5e1b9607c58f357f05b9a6f1
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-05-01 12:40:09 +0530

    refactor `scrapeItemsList` to its own file

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,22 +4,14 @@ import Header from "./../components/HomePage/Header";
 import ScrapItems from "./../components/HomePage/ScrapItems";
 import GrandTotal from "./../components/HomePage/GrandTotal";
 import Footer from "./../components/HomePage/Footer";
-
-const scrapItems = [
-	{ id: 1, name: "plastic", earningPerKg: 15, earnings: 0 },
-	{ id: 2, name: "cardboard", earningPerKg: 8, earnings: 0 },
-	{ id: 3, name: "paper", earningPerKg: 10, earnings: 0 },
-	{ id: 4, name: "steel", earningPerKg: 40, earnings: 0 },
-	{ id: 5, name: "iron", earningPerKg: 30, earnings: 0 },
-	{ id: 6, name: "german", earningPerKg: 120, earnings: 0 },
-];
+import { scrapItemsList } from "../scrapItemsList";
 
 function roundToNearestTenth(number) {
 	return Math.round(number * 10) / 10;
 }
 
 function HomePage() {
-	const [items, setItems] = useState(scrapItems);
+	const [items, setItems] = useState(scrapItemsList);
 	const newTotalEarnings = items.reduce(
 		(accumulator, item) => roundToNearestTenth(accumulator + item.earnings),
 		0
---
+++ src/scrapItemsList.jsx
@@ -0,0 +1,8 @@
+export const scrapItemsList = [
+	{ id: 1, name: "plastic", earningPerKg: 15, earnings: 0 },
+	{ id: 2, name: "cardboard", earningPerKg: 8, earnings: 0 },
+	{ id: 3, name: "paper", earningPerKg: 10, earnings: 0 },
+	{ id: 4, name: "steel", earningPerKg: 40, earnings: 0 },
+	{ id: 5, name: "iron", earningPerKg: 30, earnings: 0 },
+	{ id: 6, name: "german", earningPerKg: 120, earnings: 0 },
+];
