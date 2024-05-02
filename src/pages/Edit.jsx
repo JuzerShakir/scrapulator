588c85383e2295cbf05d05444b8898422c23d9e2
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-05-02 14:21:56 +0530

    add prop validations

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,6 +1,12 @@
+import PropTypes from "prop-types";
 import Header from "./../components/Edit/Header";
 import ScrapItems from "../components/Edit/ScrapItems";
 import Footer from "./../components/Footer";
+
+Edit.propTypes = {
+	items: PropTypes.array.isRequired,
+	setItems: PropTypes.func.isRequired,
+};
 
 function Edit({ items, setItems }) {
 	return (
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
+import PropTypes from "prop-types";
 import Header from "./../components/HomePage/Header";
 import ScrapItems from "./../components/HomePage/ScrapItems";
 import GrandTotal from "./../components/HomePage/GrandTotal";
 import Footer from "./../components/Footer";
+
+HomePage.propTypes = {
+	items: PropTypes.array.isRequired,
+	setItems: PropTypes.func.isRequired,
+};
 
 function HomePage({ items, setItems }) {
 	return (
