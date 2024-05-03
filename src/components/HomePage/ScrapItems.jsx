5e849c25583daad174851880a869f1d8f3575dc5
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-05-03 10:39:51 +0530

    save user input `weight` to `scrapItemsList`

--- src/components/HomePage/ScrapItems.jsx
+++ src/components/HomePage/ScrapItems.jsx
@@ -19,6 +19,7 @@ export default function ScrapItems({ items, setItems }) {
 					? {
 							...item,
 							earnings: roundToNearestTenth(weight * item.earningPerKg),
+							weight: Number(weight),
 					  }
 					: item
 			)
