b59dea4ee20591f38d9f6f164b0bf5802103ebc6
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-05-03 12:55:05 +0530

    update stylings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 export default function Footer() {
 	return (
-		<footer className="mb-2">
+		<footer>
 			<copy className="roboto-condensed-light-italic text-xs md:text-sm tracking-wider md:tracking-wide">
-				&copy; 2024 Created with ❤️ by Juzer Shakir
+				&copy; 2024 Created with ❤️ by{" "}
+				<strong className="font-light">Juzer Shakir</strong>
 			</copy>
 		</footer>
 	);
