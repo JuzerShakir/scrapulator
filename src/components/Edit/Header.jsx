58260544b4f91e83a28245dcfca557903360f509
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-05-03 11:17:32 +0530

    add subheading to Edit page

--- src/components/Edit/Header.jsx
+++ src/components/Edit/Header.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 export default function Header() {
 	return (
 		<>
@@ -5,7 +7,34 @@ export default function Header() {
 				<h1 className="patrick-hand-sc-regular text-6xl text-yellow-700">
 					Edit Price
 				</h1>
+				<h2 className="patrick-hand-regular text-lg text-yellow-700 tracking-wider">
+					<SubHeading />
+				</h2>
 			</header>
 		</>
 	);
+}
+
+function SubHeading() {
+	const subHeadings = ["Enter custom price", "Prices update instantly"];
+
+	const [currentSubheadingIndex, setCurrentSubheadingIndex] = useState(0);
+
+	// Function to cycle through subheadings
+	const changeSubheading = () => {
+		setCurrentSubheadingIndex(
+			(prevIndex) => (prevIndex + 1) % subHeadings.length
+		);
+	};
+
+	// Change subheading every 5 seconds
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			changeSubheading();
+		}, 2500); // Change subheading every 2.5 seconds
+
+		return () => clearInterval(intervalId);
+	});
+
+	return subHeadings[currentSubheadingIndex];
 }
